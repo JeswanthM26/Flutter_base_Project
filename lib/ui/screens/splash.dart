ecac13a762fa67980a690b821d46a05e59eedadf
Dharshini-080 <dharshinivadivel2003@gmail.com>
2025-08-26 11:29:16 +0530

    Splash integrated

--- lib/ui/screens/splash.dart
+++ lib/ui/screens/splash.dart
@@ -1,37 +1,37 @@
-import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart'; // Optional, for navigation
+// import 'package:flutter/material.dart';
+// import 'package:go_router/go_router.dart'; // Optional, for navigation
 
-class FullScreenSplash extends StatefulWidget {
-  const FullScreenSplash({Key? key}) : super(key: key);
+// class FullScreenSplash extends StatefulWidget {
+//   const FullScreenSplash({Key? key}) : super(key: key);
 
-  @override
-  State<FullScreenSplash> createState() => _FullScreenSplashState();
-}
+//   @override
+//   State<FullScreenSplash> createState() => _FullScreenSplashState();
+// }
 
-class _FullScreenSplashState extends State<FullScreenSplash> {
-  @override
-  void initState() {
-    super.initState();
+// class _FullScreenSplashState extends State<FullScreenSplash> {
+//   @override
+//   void initState() {
+//     super.initState();
 
-    Future.delayed(const Duration(seconds: 3), () {
-      if (mounted) {
-        context.go('/onboarding'); // Navigate to onboarding screen after splash
-      }
-    });
-  }
+//     Future.delayed(const Duration(seconds: 3), () {
+//       if (mounted) {
+//         context.go('/onboarding'); // Navigate to onboarding screen after splash
+//       }
+//     });
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          Image.asset(
-            'assets/images/Splash.png', // Your full-screen splash image
-            fit: BoxFit.cover,
-          ),
-        ],
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Stack(
+//         fit: StackFit.expand,
+//         children: [
+//           Image.asset(
+//             'assets/images/Splash.png', // Your full-screen splash image
+//             fit: BoxFit.cover,
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
